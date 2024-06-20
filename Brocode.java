d3a703774321ea348d40e9153d94941fa3f85612
KABANOFESTO <kabanofesto1@gmail.com>
2024-06-20 14:54:13 +0200

    starter

--- Brocode.java
+++ Brocode.java
@@ -1,9 +1,24 @@
+import java.util.Scanner;
+
 /**
  * Brocode
  */
 public class Brocode {
 
     public static void main(String args[]) {
+        double x;
+        double y;
+        double z;
+        Scanner scanner = new Scanner(System.in);
 
+        System.out.println("enter side x:");
+        x = scanner.nextDouble();
+        System.out.println("Enter side y:");
+        y = scanner.nextDouble();
+        z = Math.sqrt((x * x) + (y * y));
+
+        System.out.println("the hypotenuse equal:" + z);
+
+        scanner.close();
     }
 }
---
+++ RandomTest.java
@@ -0,0 +1,13 @@
+import java.util.Random;
+
+public class RandomTest {
+    public static void main(String args[]) {
+        Random random = new Random();
+
+        // int x = random.nextInt(6) + 1;
+        double y=random.nextDouble();
+
+        System.out.println(y);
+    }
+
+}
